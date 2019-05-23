694c85a4c8713f8fe0cdc5e4bbf33b9591aecf45
Darwin Lyons <whoischarles@gmail.com>
2019-05-23 09:56:52 -0400

    made one query for both era and genre, set up that query in a function

--- scripts/script.js
+++ scripts/script.js
@@ -8,41 +8,60 @@ app.apiKey = `23df60022b298f4d2aa0b8828774fb95`;
 
 
 app.eraMappings = {
-    old: `primary_release_date.lte=1980`,
-    eighties: `primary_release_date.lte=1989&primary_release_date.gte=1980`,
-    nineties: `primary_release_date.lte=1999&primary_release_date.gte=1990`,
-    modern: `primary_release_date.gte=1999`
+    old: {
+        start: `1910`,
+        end: `1979`
+    },
+    eighties: {
+        start: `1980`,
+        end: `1989`
+    },
+    nineties: {
+        start: `1990`,
+        end: `1999`
+    },
+    modern: {
+        start: `2000`,
+        end: `2019`
+    }
 };
 
 //call AJAX to get genres
-$.ajax({
-    url: `https://api.themoviedb.org/3/genre/movie/list`,
-    method: `GET`,
-    dataType: `json`,
-    data: {
-        api_key: app.apiKey,
-        language: `en-US`
-    }
-}).then( function(result){
-    //save genre IDS in an array in namespace
-    app.genreIDs = result;
-} );
+// $.ajax({
+//     url: `https://api.themoviedb.org/3/genre/movie/list`,
+//     method: `GET`,
+//     dataType: `json`,
+//     data: {
+//         api_key: app.apiKey,
+//         language: `en-US`
+//     }
+// }).then( function(result){
+//     //save genre IDS in an array in namespace
+//     app.genreIDs = result;
+// } );
 
 
 //query API for movies that match genre and era, first page only
-$.ajax({
-    url: `https://api.themoviedb.org/3/discover/movie?${app.eraSearchString}`,
-    method: `GET`,
-    dataType: `json`,
-    data: {
-        api_key: app.apiKey,
-        language: `en-US`,
-        with_genres: app.genreIDs
-    }
-}).then(function (result) {
-    //save genre IDS in an array in namespace
-    console.log(result);
-});
+app.$movieQuery = function (genre, startDate, endDate){
+
+    $.ajax({
+        url: `https://api.themoviedb.org/3/discover/movie?${app.eraSearchString}`,
+        method: `GET`,
+        dataType: `json`,
+        data: {
+            api_key: app.apiKey,
+            language: `en-US`,
+            with_genres: genre,
+            'release_date.gte': startDate,
+            'release_date.lte': endDate
+        }
+    
+    }).then(function(results){
+        return results
+    })
+} 
+    
+
 
 
 //get user's selection for genre category
@@ -53,7 +72,7 @@ $(`#genre`).on(`change`, function(){
 //get user's selection for era
 $(`#era`).on(`change`, function(){
     app.eraPicked = $(this).val();
-    app.eraSearchString = app.eraMappings[app.eraPicked];
+    
 });
 
 
@@ -71,5 +90,5 @@ $(`#era`).on(`change`, function(){
 
 //Document ready check
 $(function () {
-    
+    app.$movieQuery(app.genrePicked, app.eraMappings[app.eraPicked].start, app.eraMappings[app.eraPicked].end);
 })
